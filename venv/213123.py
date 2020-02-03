98b5552333acf9758a26ddb04307fff88aadecbb
NigtCr0wler <vofedotenko@gmail.com>
2020-02-03 21:03:20 +0200

    HomeWork3

---
+++ venv/213123.py
@@ -0,0 +1,16 @@
+from random import randint as rnd
+
+a = (int(input( 'Введите длину списка:')))
+b = (int(input('Введите максимальное значение элемента списка: ')))
+
+def Good (a,b):
+
+    list = []
+
+    for i in range(a):
+        list.append(rnd(0,b))
+    return list
+
+print(Good(a,b))
+
+print("Good :D")
