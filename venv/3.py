a8dd92bebf7c6baebcbcea272519a1b200cffa77
NigtCr0wler <vofedotenko@gmail.com>
2020-02-03 23:15:08 +0200

    Домашнее задание №3 работа №3

--- venv/3.py
+++ venv/3.py
@@ -1,6 +1,6 @@
 from random import randint as rnd
 
 a = rnd(0, 999)
-t = dict.fromkeys([str(input("Введіть будь-яку фразу та натисніть Enter: "))], a)
+t = dict.fromkeys([str(input('Введите слово'))], a)
 
 print(t)
